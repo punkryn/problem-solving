8e2fc7930d68afb66670cb89f62671ba4e7ba09c
박성훈 <gns816715@gmail.com>
2022-11-27 11:55:38 +0900

    [Gold I] Title: 시니컬한 개구리, Time: 1292 ms, Memory: 268336 KB -BaekjoonHub

--- 백준/Gold/25189. 시니컬한 개구리/시니컬한 개구리.py
+++ 백준/Gold/25189. 시니컬한 개구리/시니컬한 개구리.py
@@ -5,61 +5,47 @@ si = sys.stdin.readline
 mis = lambda: map(int, si().split())
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
-INF = float('inf')
-
-def bfs(x, y, v, adj):
-    q = deque([(x, y)])
-    v[x][y] = 1
-
-    while q:
-        x, y = q.popleft()
-        
-        for nx, ny in adj[x][y]:
-            if v[nx][ny] != INF: continue
-            v[nx][ny] = v[x][y] + 1
-            q.append((nx, ny))
 
 if __name__ == '__main__':
     n, m = mis()
     rf, cf, rh, ch = mis()
     rf -= 1; cf -= 1; rh -= 1; ch -= 1
     MAP = [list(mis()) for _ in range(n)]
-    adj = [[[] for _ in range(m)] for _ in range(n)]
-    rev_adj = [[[] for _ in range(m)] for _ in range(n)]
-
-    if rf == rh and cf == ch:
-        print(0)
-        exit()
-
-    for i in range(n):
-        for j in range(m):
-            for k in range(4):
-                nx = i + dx[k] * MAP[i][j]
-                ny = j + dy[k] * MAP[i][j]
-                if not (0 <= nx < n and 0 <= ny < m): continue
-                adj[i][j].append((nx, ny))
-                rev_adj[nx][ny].append((i, j))
-
     
-    graph = [[INF] * m for _ in range(n)]
-    rev_graph = [[INF] * m for _ in range(n)]
+    v = [[[-1] * 2 for _ in range(m)] for _ in range(n)]
+    row = [False] * n
+    col = [False] * m
     
-    bfs(rf, cf, graph, adj)
-    bfs(rh, ch, rev_graph, rev_adj)
-
-
-    ans = INF
-    for i in range(n):
-        x1 = y1 = INF
-        for j in range(m):
-            x1 = min(x1, graph[i][j])
-            y1 = min(y1, rev_graph[i][j])
-        ans = min(ans, x1 + y1 - 1)
+    q = deque([(rf, cf, 0)])
+    v[rf][cf][0] = 0
     
-    for j in range(m):
-        x1 = y1 = INF
-        for i in range(n):
-            x1 = min(x1, graph[i][j])
-            y1 = min(y1, rev_graph[i][j])
-        ans = min(ans, x1 + y1 - 1)
-    print(ans if ans != INF else -1)
+    while q:
+        x, y, flag = q.popleft()
+
+        if x == rh and y == ch:
+            print(v[x][y][flag])
+            exit()
+        
+        for i in range(4):
+            nx = x + dx[i] * MAP[x][y]
+            ny = y + dy[i] * MAP[x][y]
+
+            if not (0 <= nx < n and 0 <= ny < m): continue
+            if v[nx][ny][flag] != -1: continue
+            v[nx][ny][flag] = v[x][y][flag] + 1
+            q.append((nx, ny, flag))
+        
+        if flag: continue
+        if not row[x]:
+            row[x] = True
+            for i in range(m):
+                if v[x][i][1] != -1: continue
+                v[x][i][1] = v[x][y][0] + 1
+                q.append((x, i, 1))
+        if not col[y]:
+            col[y] = True
+            for i in range(n):
+                if v[i][y][1] != -1: continue
+                v[i][y][1] = v[x][y][0] + 1
+                q.append((i, y, 1))
+    print(-1)

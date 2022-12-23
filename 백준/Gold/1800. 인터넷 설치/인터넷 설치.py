75966274429e7c2d46d9aebec6662efa9677757b
박성훈 <gns816715@gmail.com>
2022-12-23 17:22:10 +0900

    [Gold I] Title: 인터넷 설치, Time: 316 ms, Memory: 120264 KB -BaekjoonHub

--- 백준/Gold/1800. 인터넷 설치/인터넷 설치.py
+++ 백준/Gold/1800. 인터넷 설치/인터넷 설치.py
@@ -10,38 +10,28 @@ dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
 def deter(mid):
-    q = [(0, 1, 0)]
-    v = [[INF] * (k + 1) for _ in range(n + 1)]
-    v[1][0] = 0
+    q = [(0, 1)]
+    v = [INF] * (n + 1)
+    v[1] = 0
     
     while q:
-        cur_cost, cur, cur_cnt = heappop(q)
+        cur_cnt, cur = heappop(q)
 
-        if v[cur][cur_cnt] < cur_cost:
+        if v[cur] < cur_cnt:
             continue
 
         for nxt, cost in graph[cur]:
             if cost > mid:
-                nxt_cnt = cur_cnt + 1
-                if nxt_cnt > k: continue
-                nxt_cost = cur_cost + 0
-                if v[nxt][nxt_cnt] > nxt_cost:
-                    v[nxt][nxt_cnt] = nxt_cost
-                    heappush(q, (nxt_cost, nxt, nxt_cnt))
-            else:
-                nxt_cnt = cur_cnt + 1
-                nxt_cost = cur_cost + 0
-                if nxt_cnt <= k and v[nxt][nxt_cnt] > nxt_cost:
-                    v[nxt][nxt_cnt] = nxt_cost
-                    heappush(q, (nxt_cost, nxt, nxt_cnt))
+                if v[nxt] > cur_cnt + 1:
+                    v[nxt] = cur_cnt + 1
+                    heappush(q, (cur_cnt + 1, nxt))
+                continue
                 
-                nxt_cnt = cur_cnt
-                nxt_cost = cur_cost + cost
-                if nxt_cnt > k: continue
-                if v[nxt][nxt_cnt] > nxt_cost:
-                    v[nxt][nxt_cnt] = nxt_cost
-                    heappush(q, (nxt_cost, nxt, nxt_cnt))
-    return min(v[n]) != INF
+            if v[nxt] > cur_cnt:
+                v[nxt] = cur_cnt
+                heappush(q, (cur_cnt, nxt))
+        
+    return v[n] <= k and v[n] != INF
                 
 
 if __name__ == '__main__':

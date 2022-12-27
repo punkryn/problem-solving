5dcf272d475dc874201cf0cb4fbcbbd504fc30a2
박성훈 <gns816715@gmail.com>
2022-12-27 13:40:40 +0900

    [Gold II] Title: 칵테일, Time: 136 ms, Memory: 114276 KB -BaekjoonHub

---
+++ 백준/Gold/1033. 칵테일/칵테일.py
@@ -0,0 +1,42 @@
+# https://www.acmicpc.net/problem/1033
+import sys
+from collections import deque
+from math import gcd
+# sys.setrecursionlimit(int(1e9))
+si = sys.stdin.readline
+mis = lambda: map(int, si().split())
+INF = float('inf')
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+def DFS(x):
+    if len(graph[x]) == 1 and v[graph[x][0][0]]:
+        return 1
+
+    ret = 1
+    for nxt, p, q in graph[x]:
+        if v[nxt]: continue
+        v[nxt] = True
+        ret *= DFS(nxt) * p
+    return ret
+
+if __name__ == '__main__':
+    n = int(si())
+    graph = [[] for _ in range(n)]
+    for _ in range(n - 1):
+        a, b, p, q = mis()
+        graph[a].append((b, p, q))
+        graph[b].append((a, q, p))
+    
+    ans = [1] * n
+    for i in range(n):
+        v = [False] * n
+        v[i] = True
+        tmp = 1
+        for nxt, p, q in graph[i]:
+            v[nxt] = True
+            tmp *= DFS(nxt) * p
+        ans[i] = tmp
+    g = gcd(*ans)
+    
+    print(*map(lambda x: x // g, ans))

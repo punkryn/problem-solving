ad59691a8725621932e8581cf25ac1bd9552bc68
박성훈 <gns816715@gmail.com>
2023-02-12 14:50:47 +0900

    [Silver I] Title: 단지번호붙이기, Time: 88 ms, Memory: 32444 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -0,0 +1,47 @@
+# https://www.acmicpc.net/problem/2667
+
+import sys
+from collections import deque
+si = sys.stdin.readline
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+def bfs(x, y, visited, n, num, MAP):
+    q = deque()
+    q.append((x, y))
+    visited[x][y] = num
+    cnt = 1
+    while q:
+        x_, y_ = q.popleft()
+        for i in range(4):
+            nx = x_ + dx[i]
+            ny = y_ + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < n:
+                if visited[nx][ny] == 0 and MAP[nx][ny] == '1':
+                    visited[nx][ny] = num
+                    cnt += 1
+                    q.append((nx, ny))
+    return cnt
+
+def main():
+    n = int(si())
+    MAP = [si().strip() for _ in range(n)]
+
+    visited = [[0] * n for _ in range(n)]
+    
+    num = 0
+    ans = []
+    for i in range(n):
+        for j in range(n):
+            if MAP[i][j] == '1' and visited[i][j] == 0:
+                num += 1
+                ans.append(bfs(i, j, visited, n, num, MAP))
+
+    # for v in visited:
+    #     print(v)
+    print(num)    
+    for a in sorted(ans):
+        print(a)
+
+if __name__ == '__main__':
+    main()

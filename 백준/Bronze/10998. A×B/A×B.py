f383798bd2174aa2d597575168799fcb70757d93
박성훈 <gns816715@gmail.com>
2023-03-18 00:29:59 +0900

    [Bronze V] Title: A×B, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.py
@@ -0,0 +1,11 @@
+import sys
+# sys.setrecursionlimit(int(1e9))
+si = sys.stdin.readline
+mis = lambda: map(int, si().split())
+INF = float('inf')
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+if __name__ == '__main__':
+    a, b = mis()
+    print(a * b)

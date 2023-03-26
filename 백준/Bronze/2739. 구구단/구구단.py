102e69b5a6d9c68e22c96472e11b1f694caa38f3
박성훈 <gns816715@gmail.com>
2023-03-26 20:55:44 +0900

    [Bronze V] Title: 구구단, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.py
@@ -0,0 +1,13 @@
+import sys
+# sys.setrecursionlimit(int(1e9))
+si = sys.stdin.readline
+mis = lambda: map(int, si().split())
+INF = float('inf')
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+if __name__ == '__main__':
+    n = int(si())
+
+    for i in range(1, 10):
+        print(f'{n} * {i} = {n*i}')

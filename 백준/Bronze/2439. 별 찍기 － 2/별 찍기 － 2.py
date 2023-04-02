2530ed12426c7ba980edf9501d2a8174f1cf48ff
박성훈 <gns816715@gmail.com>
2023-04-02 10:26:24 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
@@ -0,0 +1,16 @@
+import sys
+# sys.setrecursionlimit(int(1e9))
+si = sys.stdin.readline
+mis = lambda: map(int, si().split())
+INF = float('inf')
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+if __name__ == '__main__':
+    n = int(si())
+    for i in range(1, n + 1):
+        for j in range(n - i):
+            print(end=' ')
+        for j in range(i):
+            print('*', end='')
+        print()

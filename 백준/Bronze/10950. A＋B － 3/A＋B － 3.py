a91e5d390472d2cac54e482438b8d6866f60e9f1
박성훈 <gns816715@gmail.com>
2023-04-04 18:05:08 +0900

    [Bronze V] Title: A+B - 3, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
@@ -0,0 +1,12 @@
+import sys
+# sys.setrecursionlimit(int(1e9))
+si = sys.stdin.readline
+mis = lambda: map(int, si().split())
+INF = float('inf')
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+if __name__ == '__main__':
+    for _ in range(int(si())):
+        a, b = mis()
+        print(a + b)

45edc2e44e71ff182e9627879b2067836aed9d3b
박성훈 <gns816715@gmail.com>
2023-04-05 20:52:17 +0900

    [Bronze III] Title: 최소, 최대, Time: 372 ms, Memory: 154704 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -0,0 +1,12 @@
+import sys
+# sys.setrecursionlimit(int(1e9))
+si = sys.stdin.readline
+mis = lambda: map(int, si().split())
+INF = float('inf')
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+if __name__ == '__main__':
+    n = int(si())
+    A = list(mis())
+    print(min(A), max(A))

058168952083104f7dd9500169f58e1aa537c2b8
박성훈 <gns816715@gmail.com>
2023-05-05 13:19:36 +0900

    [Gold IV] Title: 최대 정사각형, Time: 4052 ms, Memory: 70016 KB -BaekjoonHub

---
+++ 백준/Gold/4095. 최대 정사각형/최대 정사각형.go
@@ -0,0 +1,79 @@
+// https://www.acmicpc.net/problem/4095
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+var dx = [4]int{-1, 0, 1, 0}
+var dy = [4]int{0, 1, 0, -1}
+
+var (
+	n, m int
+	matrix [][]int
+	dp [][]int
+)
+func main() {
+	defer writer.Flush()
+
+	for {
+		fmt.Fscan(reader, &n, &m)
+		if n == 0 && m == 0 {
+			return
+		}
+
+		matrix = make([][]int, n + 1)
+		dp = make([][]int, n + 1)
+		for i := 0; i <= n; i++ {
+			matrix[i] = make([]int, m + 1)
+			dp[i] = make([]int, m + 1)
+		}
+
+		for i := 1; i <= n; i++ {
+			for j := 1; j <= m; j++ {
+				fmt.Fscan(reader, &matrix[i][j])
+			}
+		}
+
+		ans := 0
+		for i := 1; i <= n; i++ {
+			for j := 1; j <= m; j++ {
+				if matrix[i][j] == 0 { continue }
+				dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+				ans = max(ans, dp[i][j])
+			}
+		}
+		fmt.Println(ans)
+	}
+}
+type IntType interface {
+	int | int64 | int32
+}
+func min[it IntType](x ...it) it {
+	var ret it = x[0]
+	for _, v := range x[1:] {
+		if ret > v {
+			ret = v
+		}
+	}
+	return ret
+}
+func max[it IntType](x ...it) it {
+	var ret it = x[0]
+	for _, v := range x[1:] {
+		if ret < v {
+			ret = v
+		}
+	}
+	return ret
+}
+func sum[it IntType](x ...it) it {
+	var ret it = 0
+	for _, v := range x {
+		ret += v
+	}
+	return ret
+}

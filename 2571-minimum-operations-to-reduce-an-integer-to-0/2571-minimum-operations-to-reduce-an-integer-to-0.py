6ab26255c95f11fbfaec2bbd354a946c78c9b7ef
박성훈 <gns816715@gmail.com>
2023-12-23 17:07:54 +0900

    Time: 31 ms (91.66%), Space: 17.3 MB (6.50%) - LeetHub

---
+++ 2571-minimum-operations-to-reduce-an-integer-to-0/2571-minimum-operations-to-reduce-an-integer-to-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minOperations(self, n: int) -> int:
+        ans = 0
+        while n > 0:
+            if n % 2 == 0:
+                n >>= 1
+            elif (n & 2) > 0:
+                n += 1
+                ans += 1
+            else:
+                n >>= 2
+                ans += 1
+        return ans

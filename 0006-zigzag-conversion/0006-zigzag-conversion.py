3f80293eb8cfb0b6abb3a7a6a7e8962a78192caa
박성훈 <gns816715@gmail.com>
2024-01-04 10:31:23 +0900

    Time: 54 ms (78.28%), Space: 17.4 MB (20.07%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -2,24 +2,13 @@ class Solution:
     def convert(self, s: str, numRows: int) -> str:
         if numRows == 1:
             return s
-        MAX_N = 1005
-        matrix = [[''] * MAX_N for _ in range(MAX_N)]
-
-        x, y = 0, 0
-        flag = 0
-        cnt = 0
-
-        while cnt < len(s):
-            matrix[x][y] = s[cnt]
-
-            if flag == 0:
-                x += 1
-            else:
-                x -= 1
-                y += 1
         
-            cnt += 1
-            if cnt % (numRows - 1) == 0:
-                flag = (flag + 1) % 2
-        
-        return ''.join([''.join(mat) for mat in matrix[:numRows]])
+        ans = [''] * numRows
+        bias = (numRows - 1) * 2
+        for i in range(len(s)):
+            mod = i % bias
+            if mod >= numRows:
+                ans[bias - mod] += s[i]
+                continue
+            ans[mod] += s[i]
+        return ''.join(ans)

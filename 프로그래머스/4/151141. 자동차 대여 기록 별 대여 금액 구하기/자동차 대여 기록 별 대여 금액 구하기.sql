149b2de16eb96cca9894f828ae8309798e8e9a7b
박성훈 <gns816715@gmail.com>
2024-07-07 00:28:59 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -0,0 +1,35 @@
+with plan7 as (
+    select discount_rate from CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+    where car_type = '트럭' and duration_type = '7일 이상'
+), plan30 as (
+    select discount_rate from CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+    where car_type = '트럭' and duration_type = '30일 이상'
+), plan90 as (
+    select discount_rate from CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+    where car_type = '트럭' and duration_type = '90일 이상'
+)
+SELECT
+    history_id,
+    floor((
+        case
+            when datediff(joined.end_date, joined.start_date) + 1 >= 90
+                then (100 - CAST(REPLACE(plan90.discount_rate, '%', '') AS DECIMAL(5,2))) / 100
+            when datediff(joined.end_date, joined.start_date) + 1 >= 30
+                then (100 - CAST(REPLACE(plan30.discount_rate, '%', '') AS DECIMAL(5,2))) / 100
+            when datediff(joined.end_date, joined.start_date) + 1 >= 7
+                then (100 - CAST(REPLACE(plan7.discount_rate, '%', '') AS DECIMAL(5,2))) / 100
+            else
+                1
+        end
+    ) * joined.daily_fee * (datediff(joined.end_date, joined.start_date) + 1)) FEE
+from (
+    select history.*, car.car_type, car.daily_fee
+    from CAR_RENTAL_COMPANY_RENTAL_HISTORY history
+    join (
+        select *
+        from CAR_RENTAL_COMPANY_CAR
+        where car_type = '트럭'
+    ) car
+    on history.car_id = car.car_id
+) joined, plan7, plan30, plan90
+order by FEE desc, history.history_id desc;

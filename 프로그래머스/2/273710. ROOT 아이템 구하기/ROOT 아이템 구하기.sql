6059f95a7d6b787f7e964dece60ebd62d693ff58
박성훈 <gns816715@gmail.com>
2024-07-07 14:43:47 +0900

    [level 2] Title: ROOT 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
@@ -0,0 +1,7 @@
+-- 코드를 작성해주세요
+select info.item_id, info.item_name
+from item_info info
+join item_tree tree
+on info.item_id = tree.item_id
+where tree.parent_item_id is null
+order by info.item_id asc;
